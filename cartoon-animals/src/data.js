317823ca25824ad2aa2b50d81e5c882157c52061
Jeremy Sabina <Jeremy_Sabina@yahoo.com>
2024-11-16 22:33:06 -0500

    Need to add the right data attachments

---
+++ cartoon-animals/src/data.js
@@ -0,0 +1,11 @@
+const data = [
+    {
+        "id": 1,
+        "name": "Winnie the Pooh",
+        "animal": "Bear",
+        "accomplishments": ["Helping Rabbit", "Giving Eyore his tail back", "Being Christopher Robin's friend"],
+        "url": "https://en.wikipedia.org/wiki/Winnie_the_Pooh_%28Disney_character%29#/media/File:Winniethepooh.png"
+    } ,
+]
+
+export default data;
